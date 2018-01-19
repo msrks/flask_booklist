3918ba406a34594a9590f920787aec36403b54cc
msrks <mshrrks@gmail.com>
2018-01-19 12:07:30 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## 書籍購入リスト管理アプリ
+
+初回はデータベースを新規に作成
+
+```
+$ python
+>>> from app import db
+>>> db.create_all()
+```
+
+アプリの起動は以下のコマンドを叩いて、http://localhost:5000にアクセス
+
+```bash
+$ python app.py
+```
---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request
+from flask import render_template
+from flask import redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    price = db.Column(db.Integer)
+
+    def __init__(self, title, price):
+        self.title = title
+        self.price = price
+
+    def __repr__(self):
+        return '<title %s>' % self.title
+
+
+@app.route('/')
+def books_list():
+    books = Book.query.all()
+    total_price = sum([i.price for i in books])
+    return render_template('books_list.html', books=books, total_price=total_price)
+
+
+@app.route('/add', methods=['POST'])
+def add_book():
+    title = request.form['title']
+    price = request.form['price']
+    if not title or not price:
+        return 'Error'
+
+    try:
+        price = int(price)
+    except:
+        return 'Error'
+
+    book = Book(title, price)
+    db.session.add(book)
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route('/delete/<int:book_id>')
+def delete_book(book_id):
+    book = Book.query.get(book_id)
+    if not book:
+        return redirect('/')
+
+    db.session.delete(book)
+    db.session.commit()
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
